e2beface3929ecd42142511099b2b5123e1775f4
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2023-09-03 14:31:03 +0700

    Time: 105 ms (43.73%), Space: 9.3 MB (65.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,9 +1,21 @@
+// func containsDuplicate(nums []int) bool {
+//   sort.Ints(nums)
+//     for i := 1; i < len(nums); i++ {
+//         if nums[i] == nums[i-1] {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
 func containsDuplicate(nums []int) bool {
-  sort.Ints(nums)
-    for i := 1; i < len(nums); i++ {
-        if nums[i] == nums[i-1] {
-            return true
-        }
+    seen := make(map[int]bool)
+    
+    for _, num := range nums {
+        if seen[num] {
+            return true 
+        } 
+        seen[num] = true
     }
     return false
 }
