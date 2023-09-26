55dc89dc9fe5054fc7176954bdb3febf7626d2ed
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2023-09-26 09:22:04 +0700

    Time: 2 ms (38.55%), Space: 2.5 MB (31.37%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -0,0 +1,23 @@
+func mergeAlternately(word1 string, word2 string) string {
+    hasil := ""
+    
+    i , j := 0 , 0
+    
+    for i < len(word1) && j < len(word2) {
+        hasil += string(word1[i]) + string(word2[j])
+        i++
+        j++
+    }
+    
+    for i < len(word1) {
+        hasil += string(word1[i])
+        i++
+    }
+    
+    for j < len(word2) {
+        hasil += string(word2[j])
+        j++
+    }
+    
+    return hasil
+}
