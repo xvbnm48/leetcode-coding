854a6b7eb688b07d93f41d70e3299013dc701ccc
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2023-10-05 16:26:54 +0700

    Time: 110 ms (68.85%), Space: 2 MB (36.98%) - LeetHub

--- 0079-word-search/0079-word-search.go
+++ 0079-word-search/0079-word-search.go
@@ -1,10 +1,7 @@
 func exist(board [][]byte, word string) bool {
-    // Membuat array bool untuk melacak sel-sel yang sudah dikunjungi
-    visited := make([][]bool, len(board))
-    for i := range visited {
-        visited[i] = make([]bool, len(board[0]))
-    }
-
+    // Mendapatkan panjang dan lebar dari matriks
+    m, n := len(board), len(board[0])
+    
     // Fungsi rekursif untuk melakukan pencarian
     var search func(i, j, k int) bool
     search = func(i, j, k int) bool {
@@ -13,27 +10,30 @@ func exist(board [][]byte, word string) bool {
             return true
         }
         
-        // Jika posisi saat ini di luar batas matriks atau sel telah dikunjungi,
+        // Jika posisi saat ini di luar batas matriks,
         // atau huruf di sel tidak sesuai dengan huruf yang sedang dicari, kembalikan false
-        if i < 0 || i >= len(board) || j < 0 || j >= len(board[0]) || visited[i][j] || board[i][j] != word[k] {
+        if i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k] {
             return false
         }
         
-        // Tandai sel saat ini sebagai sudah dikunjungi
-        visited[i][j] = true
+        // Simpan karakter yang ada di sel saat ini
+        temp := board[i][j]
+        
+        // Tandai sel saat ini sebagai karakter yang tidak valid (agar tidak digunakan lagi)
+        board[i][j] = '#'
         
         // Coba bergerak ke sel-sel tetangga dalam empat arah (atas, bawah, kiri, kanan)
         found := search(i+1, j, k+1) || search(i-1, j, k+1) || search(i, j+1, k+1) || search(i, j-1, k+1)
         
-        // Setelah selesai pencarian dalam satu arah, kembalikan status visited ke false
-        visited[i][j] = false
+        // Kembalikan karakter sel ke nilai aslinya
+        board[i][j] = temp
         
         return found
     }
 
     // Cari huruf pertama dari kata pada setiap sel dalam matriks
-    for i := range board {
-        for j := range board[i] {
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
             if board[i][j] == word[0] && search(i, j, 0) {
                 return true
             }
