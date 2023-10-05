05be8693ae0f5d8b1b66dea303dda0a86e4ebeb6
M Fariz Wisnu prananda <muhamadfarizwisnuprananda@gmail.com>
2023-10-05 16:13:39 +0700

    Time: 23 ms (40.07%), Space: 7.1 MB (42.71%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,14 @@
+func moveZeroes(nums []int)  {
+    if len(nums) == 0 {
+		return
+	}
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			if i != j {
+				nums[i], nums[j] = nums[j], nums[i]
+			} 
+			j++
+		}
+	}
+}
